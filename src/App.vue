4b58222c045989a4632f1c3331de2d713bdbb5b3
Karol Mąkowski <karol-roch@wp.pl>
2018-01-07 12:45:10 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  	<router-view></router-view>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="sass"> 
+  #_app
+    font-family: 'Lato', sans-serif
+    font-style: normal
+    font-weight: 400
+    line-height: 1
+    display: flex
+    justify-content: center
+    //min-height: 100vh
+
+</style>
