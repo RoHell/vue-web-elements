16af775431dba4de28f804fd4100b25eebfea29e
Karol Mąkowski <karol-roch@wp.pl>
2018-01-08 23:25:37 +0100

    removal moved filas, router preparation for future

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import componentsView from '../components/componentsView'
+import componentsView from '../components/views/componentsView'
+// import buttonComponent from '../components/elements/button'
+// import tooltipComponent from '../components/elements/tooltip'
+// import alertComponent from '../components/elements/alert'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
-    routes: [{
-        path: '/',
-        name: 'componentsView',
-        component: componentsView
-    }]
+    routes: [
+    	{
+	        path: '/',
+	        name: 'componentsView',
+	        component: componentsView,
+	        // children: [
+	        // 	{
+	        // 		path: 'button',
+	        // 		name: 'button',
+	        // 		component: buttonComponent,
+	        // 	},
+	        // 	{
+	        // 		path: 'tooltip',
+	        // 		name: 'tooltip',
+	        // 		component: tooltipComponent,
+	        // 	},
+	        // 	{
+	        // 		path: 'alert',
+	        // 		name: 'alert',
+	        // 		component: alertComponent,
+	        // 	},
+
+	        // ]
+    	}
+    ]
 })
