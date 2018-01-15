9945ef7e0399f7af72fd6f3d2da322fcfba6d569
RoHell <karol-roch@wp.pl>
2018-01-15 22:56:50 +0100

    animation

--- src/components/elements/DfLoader.vue
+++ src/components/elements/DfLoader.vue
@@ -21,24 +21,23 @@
   @import '../../sass/base/dimensions.sass'
   @import '../../sass/base/colors.sass'
 
+  @mixin loader-position-size($size)
+    top: calc(50% - #{$size/2})
+    left: calc(50% - #{$size/2})
+    width: $size
+    height: $size
+
   ._df-loader
     position: absolute
     width: 100%
     height: 100%
     z-index: 10
-
     div
-      @mixin loader-position-size($size)
-        top: calc(50% - #{$size/2})
-        left: calc(50% - #{$size/2})
-        width: $size
-        height: $size
-
       @include loader-position-size($button-size-normal)
       position: absolute
       box-sizing: border-box
       border-radius: 50%
-      animation: spin 1.2s cubic-bezier(0.55, 0.2, 0.55, 0.8) infinite
+      animation: spin 1.3s cubic-bezier(0.55, 0.2, 0.55, 0.8) infinite
 
       @keyframes spin
         to
@@ -50,7 +49,7 @@
 
       @each $color-type, $color, $index in $color-types-map
         &.#{$color-type}
-          border: 2px solid transparent
+          border: 3px solid transparent
           border-right-color: lighten($color, 15%)
 
       @for $i from 2 through 4
